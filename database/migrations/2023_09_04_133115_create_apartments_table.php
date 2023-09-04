dc92f44c1fe83401b0f506b50932a1b1fef7a077
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-09-04 15:54:08 +0200

    first commit

---
+++ database/migrations/2023_09_04_133115_create_apartments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 128);
+            $table->text('description');
+            $table->integer('room');
+            $table->integer('bathroom');
+            $table->integer('mq');
+            $table->text('address');
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->boolean('visible');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
