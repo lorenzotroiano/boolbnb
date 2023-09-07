2469abd10edce6db75f5941e772d647a60060e0b
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-09-07 11:04:08 +0200

    sistemati i servizi nei seeder

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -16,9 +16,9 @@ class ServiceFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => fake()->randomElement(['wifi', 'spotify', 'garage', 'youtube', 'condizionatore', 'palestra', 'bar', 'scuola', 'tv', 'idromassaggio', 'colazione', 'animali consentiti']),
-            'icon' => fake()->image(null, 80, 80),
-        ];
+        // return [
+        //     'name' => fake()->randomElement(['wifi', 'spotify', 'garage', 'youtube', 'condizionatore', 'palestra', 'bar', 'scuola', 'tv', 'idromassaggio', 'colazione', 'animali consentiti']),
+        //     'icon' => fake()->image(null, 80, 80),
+        // ];
     }
 }
--- database/seeders/ServiceTableSeeder.php
+++ database/seeders/ServiceTableSeeder.php
@@ -14,16 +14,103 @@ class ServiceTableSeeder extends Seeder
      *
      * @return void
      */
+    // public function run()
+    // {
+    //     Service::create([
+    //         'name' => 'Wifi',
+    //         'icon' => 'nome_del_file_icona_wifi.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Spotify',
+    //         'icon' => 'nome_del_file_icona_spotify.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Tv',
+    //         'icon' => 'Tv.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'FrigoBar',
+    //         'icon' => 'FrigoBar.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Idromassaggio',
+    //         'icon' => 'Idromassaggio.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Garage',
+    //         'icon' => 'Garage.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Animali',
+    //         'icon' => 'Animali.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Bevande',
+    //         'icon' => 'Bevande.png',
+    //     ]);
+
+
+    //     Service::create([
+    //         'name' => 'Palestra',
+    //         'icon' => 'Palestra.png',
+    //     ]);
+
+    //     Service::create([
+    //         'name' => 'Bar',
+    //         'icon' => 'Bar.png',
+    //     ]);
+
+    //     // Aggiungi altri servizi desiderati qui...
+
+    //     $apartments = Apartment::inRandomOrder()->limit(rand(1, 3))->get();
+    //     $services = Service::whereIn('name', ['Wifi', 'Spotify'])->get();
+
+    //     foreach ($services as $service) {
+    //         $service->apartments()->attach($apartments);
+    //     }
+    // }
+
+
     public function run()
     {
-        Service::factory()->count(10)->create();
+        // Definisci un array di dati dei servizi desiderati
+        $serviceData = [
+            ['name' => 'Wifi', 'icon' => 'nome_del_file_icona_wifi.png'],
+            ['name' => 'Spotify', 'icon' => 'nome_del_file_icona_spotify.png'],
+            ['name' => 'Tv', 'icon' => 'Tv.png'],
+            ['name' => 'FrigoBar', 'icon' => 'FrigoBar.png'],
+            ['name' => 'Idromassaggio', 'icon' => 'Idromassaggio.png'],
+            ['name' => 'Garage', 'icon' => 'Garage.png'],
+            ['name' => 'Animali', 'icon' => 'Animali.png'],
+            ['name' => 'Bevande', 'icon' => 'Bevande.png'],
+            ['name' => 'Palestra', 'icon' => 'Palestra.png'],
+            ['name' => 'Bar', 'icon' => 'Bar.png'],
+            // Puoi aggiungere altri servizi desiderati qui...
+        ];
 
-        $services = Service::factory()->count(10)->create();
+        // Loop attraverso l'array dei dati dei servizi e crea i servizi nel database
+        foreach ($serviceData as $data) {
+            Service::create($data);
+        }
 
+        // Ottieni appartamenti casuali (da 1 a 3)
+        $apartments = Apartment::inRandomOrder()->limit(rand(1, 3))->get();
+
+        // Estrai i nomi dei servizi dall'array $serviceData
+        $serviceNames = array_column($serviceData, 'name');
+
+        // Recupera i servizi corrispondenti dal database in base ai nomi estratti
+        $services = Service::whereIn('name', $serviceNames)->get();
+
+        // Associa i servizi estratti agli appartamenti
         foreach ($services as $service) {
-
-            $apartments = Apartment::inRandomOrder()->limit(rand(1, 3))->get();
-
             $service->apartments()->attach($apartments);
         }
     }
