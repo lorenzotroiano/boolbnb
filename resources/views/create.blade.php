c5920c5b1a2aaca999cc36e521c0546f8234d5ea
Pastafredda <justspotify09@gmail.com>
2023-09-08 16:35:22 +0200

    aggiunta label

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -71,7 +71,7 @@
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
 
-            
+
             <label for="visible">Visible</label>
             <br>
             <input type="radio" name="visible" id="visible-yes" value="1"> Yes
@@ -80,21 +80,20 @@
             @error('visible')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-            
+
             <br>
-
-
 
 
             @foreach ($services as $service)
                 <div class="form-check mx-auto" style="max-width: 300px">
-                    <input class="form-check-input" type="checkbox" value="{{ $service->id }}" name="services[]"
-                        id="service-{{ $service->id }}">
-                    <label class="form-check-label" for="service-{{ $service->id }}">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" value="{{ $service->id }}" name="services[]"
+                            id="service-{{ $service->id }}">
                         {{ $service->name }}
                     </label>
                 </div>
             @endforeach
+
 
             <!-- Bottone di submit per inviare il form -->
             <input class="my-3" type="submit" value="CREATE">
