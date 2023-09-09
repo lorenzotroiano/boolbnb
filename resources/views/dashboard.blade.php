01bae6fdedf3dcb4fe0922086b701de70f69444c
Manuell00 <manuell.caselli@gmail.com>
2023-09-09 12:24:05 +0200

    visible dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <h3 class="card-header">Dasboard di <span style="text-transform: capitalize">{{ Auth::user()->name }}</span> </h3>
+                <h5 class="card-header"> <b>Pubblico</b> </h5>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -23,7 +23,7 @@
                             $sorted_apartments = $apartments->sortByDesc('sponsor');
                         @endphp
                         @foreach ($sorted_apartments as $apartment)
-                            @if ($apartment->user_id === Auth::id())
+                            @if ($apartment->user_id === Auth::id() && $apartment->visible == 1)
                                 <div class="col-sm-4 my-3">
                                     <div class="card shadow @if ($apartment->sponsor) bg-info  @endif">
                                         <div class="card-body">
@@ -45,11 +45,44 @@
                 </div>
             </div>
 
+
+
+            <div class="card mt-5">
+                <h5 class="card-header"> <b>Privato</b> </h5>
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    <div class="row">
+                        @foreach ($apartments as $apartment)
+                            @if ($apartment->user_id === Auth::id() && $apartment->visible == 0 )
+                                <div class="col-sm-4 my-3">
+                                    <div class="card shadow @if ($apartment->sponsor) bg-info  @endif">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $apartment->name }}</h5>
+                                            <p class="card-text">{{ $apartment->address }}</p>
+                                            <p class="card-text">Metri quadrati: {{ $apartment->mq }}</p>
+                                            <a href="{{ route('show', $apartment->id) }}" class="btn btn-primary">Visualizza</a>
+                                            @if ($apartment ->sponsor)
+                                                <span class="btn btn-warning">Sponsorizzato</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
             {{-- Create --}}
             <div class="text-center my-3">
                 <a href="{{route('create')}}" class="btn btn-success">CREATE</a>
             </div>
-            
+
         </div>
     </div>
 </div>
