d032b54ee57bdd4931aefe92fe1dbd46d3f91fae
Manuell00 <manuell.caselli@gmail.com>
2023-09-11 14:53:13 +0200

    Search

--- front-end/src/components/ApartmentHome.vue
+++ front-end/src/components/ApartmentHome.vue
@@ -6,9 +6,19 @@ export default {
     data() {
         return {
             apartments: [],
-
+            search: '',
         }
     },
+    computed: {
+        filteredApartments() {
+            // Creo un nuovo array filtrato
+            return this.apartments.filter(apartment => {
+                // Restituisce TRUE se i valori sono verificati e FALSE se non lo sono
+                return apartment.name.toLowerCase().includes(this.search.toLowerCase());
+            });
+        }
+    },
+
     mounted() {
         axios.get('http://127.0.0.1:8000/api/v1/')
             .then(response => {
@@ -20,17 +30,23 @@ export default {
             })
     },
 };
-
-
-
 </script>
 
 <template>
     <h1>
         Lista appartamenti:
     </h1>
+
+    <!-- Creo la barra di ricerca e imposto v-model sulla variabile search -->
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Cerca..." aria-label="Cerca..."
+            aria-describedby="button-addon2" v-model="search">
+    </div>
+
+    <!-- Lista degli appartamenti -->
     <ul>
-        <li v-for="apartment in apartments" :key="apartment.id">
+        <li v-for="apartment in filteredApartments" :key="apartment.id">
+            <!-- Link alla show -->
             <router-link :to="{ name: 'apartment-show', params: { id: apartment.id } }" class="link">
                 <h2>{{ apartment.name }}</h2>
             </router-link>
