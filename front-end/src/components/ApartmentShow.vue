0e2a5007693449725bd3ad8068c01c1313f6ae9c
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-09-16 11:48:44 +0200

    aggiunto style alla show

--- front-end/src/components/ApartmentShow.vue
+++ front-end/src/components/ApartmentShow.vue
@@ -18,6 +18,7 @@ export default {
                     container: this.$refs.map, // Usa il riferimento al div della mappa
                     center: this.center, // Utilizza this.center come centro iniziale
                     zoom: 10,
+
                 });
 
                 this.map.on('load', () => {
@@ -44,44 +45,162 @@ export default {
 </script>
 
 <template>
-    <div ref="map" style="width: 50%; height: 400px;"></div>
-
-    <div>
-        <h1>Nome: {{ apartment.name }}</h1>
-        <p>ID: {{ apartment.id }}</p>
-        <p>Descrizione: {{ apartment.description }}</p>
-        <p>Stanze: {{ apartment.room }}</p>
-        <p>Bagni: {{ apartment.bathroom }}</p>
-        <p>Metri quadrati: {{ apartment.mq }}</p>
-        <p>Indirizzo: {{ apartment.address }}</p>
-        <p>Latitudine: {{ apartment.latitude }}</p>
-        <p>Longitudine: {{ apartment.longitude }}</p>
-        <p>Visibile: {{ apartment.visible }}</p>
-        <p>Sponsorizzato: {{ apartment.sponsor }}</p>
-        <img :src="apartment.cover" alt="Copertina dell'appartamento">
-
-        <!-- Servizi -->
-        <h2>Servizi:</h2>
+    <main>
         <div class="container">
-            <div class="row">
-                <div class="col-md-4" v-for="service in apartment.services" :key="service.id">
-                    <div class="card mb-3">
-                        <div class="card-body text-center">
-                            <i :class="service.icon" class="service-icon"></i>
-                            <p class="card-text">{{ service.name }}</p>
+            <h1>{{ apartment.name }}</h1>
+            <span class="address">{{ apartment.address }}</span>
+
+            <div class="flex-map">
+                <img :src="apartment.cover" alt="Copertina dell'appartamento">
+                <div ref="map" style="width: 35%; height: 400px;"></div>
+            </div>
+
+            <!-- <p>ID: {{ apartment.id }}</p> -->
+            <div class="apartment-description">
+                <h3>Info Appartamento</h3>
+                <p>{{ apartment.description }}</p>
+
+                <div class="description-flex">
+                    <button class="custom-button">
+                        <i class="fa-solid fa-house"></i>
+                        <span class="button-text">{{ apartment.room }}</span>
+                    </button>
+                    <button class="custom-button">
+                        <i class="fa-solid fa-toilet"></i>
+                        <span class="button-text">{{ apartment.bathroom }}</span>
+                    </button>
+                    <button class="custom-button">
+                        <i class="fa-brands fa-squarespace"></i>
+                        <span class="button-text">{{ apartment.mq }} Mq</span>
+                    </button>
+                </div>
+
+
+            </div>
+
+            <!-- <p>Latitudine: {{ apartment.latitude }}</p>
+        <p>Longitudine: {{ apartment.longitude }}</p> -->
+            <!-- <p>Visibile: {{ apartment.visible }}</p>
+        <p>Sponsorizzato: {{ apartment.sponsor }}</p> -->
+
+
+            <!-- Servizi -->
+            <h2 v-if="apartment && apartment.services && apartment.services.length > 0">Servizi disponibili:</h2>
+            <h2 v-else>Nessun servizio disponibile</h2>
+
+            <div class="container" v-if="apartment && apartment.services && apartment.services.length > 0">
+                <div class="row justify-content-center"> <!-- Centra gli elementi orizzontalmente -->
+                    <div class="col-md-3" v-for="service in apartment.services" :key="service.id">
+                        <!-- Riduci la larghezza delle colonne per fare spazio -->
+                        <div class="card mb-3">
+                            <div class="card-body text-center">
+                                <i :class="service.icon" class="service-icon" style="font-size: 24px;"></i>
+                                <!-- Riduci la dimensione dell'icona -->
+                                <p class="card-text">{{ service.name }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    </div>
+
+
+
+
+
+
+
+
+
+        </div>
+    </main>
 </template>
 
 
 
 
-<style scoped>
+<style scoped lang="scss">
+main {
+    background-color: chartreuse;
+
+    .container {
+        max-width: 80%;
+        margin: 0 auto;
+        padding: 70px 0;
+
+        .address {
+            font-size: 20px;
+        }
+
+        .flex-map {
+
+            margin: 50px 0;
+            display: flex;
+            justify-content: space-between;
+
+            img {
+                height: 400px;
+                width: 60%;
+            }
+        }
+
+        .apartment-description {
+            border-top: 1px black solid;
+            font-size: 20px;
+
+            padding: 45px 0;
+
+            h3 {
+                text-align: center;
+                margin-bottom: 40px;
+            }
+
+
+            .description-flex {
+                border-bottom: 1px black solid;
+                display: flex;
+                justify-content: space-around;
+                font-size: 24px;
+
+
+                padding: 40px 0;
+
+
+                .custom-button {
+                    border: none;
+                    border-radius: 12px;
+                    background-color: white;
+                    padding: 5px 10px;
+
+                    cursor: pointer;
+                    transition: background-color 0.3s ease, transform 0.2s ease;
+                    font-size: 20px;
+                    display: flex;
+                    align-items: center;
+                }
+
+                .custom-button:hover {
+                    background-color: #f0f0f0;
+                    /* Colore grigio chiaro al passaggio del mouse */
+                    transform: scale(1.05);
+                    /* Ingrandimento al passaggio del mouse */
+                }
+
+                .button-text {
+                    margin: 0 15px;
+                }
+            }
+        }
+
+        // background-color: chartreuse;
+
+
+    }
+
+}
+
+
+
 .service-icon {
     font-size: 24px;
 
