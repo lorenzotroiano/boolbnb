b24475ee6084bfd30921c47d9c9ebb2a7887c747
Manuell00 <manuell.caselli@gmail.com>
2023-09-16 14:25:18 +0200

    Rimozione style.css, installazioni librerie

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { router } from './router.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
