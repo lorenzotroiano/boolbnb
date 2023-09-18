86d5d71da0aa6062170a0b86c485c3acbe77b7ba
Sula Michele <sulamichele95@gmail.com>
2023-09-18 15:50:32 +0200

    Deleted home in laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,10 +41,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        {{-- Home --}}
+                        {{-- Home
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
-                        </li>
+                        </li> --}}
                         {{-- Dashboard --}}
                         @auth
                             <li class="nav-item">
