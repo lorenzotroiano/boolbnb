ab967dd38183eb6ed0d4835444a8b89de3bb59e2
Sula Michele <sulamichele95@gmail.com>
2023-09-26 00:33:41 +0200

    Versione provvisoria da sistemare

--- front-end/src/components/ApartmentHome.vue
+++ front-end/src/components/ApartmentHome.vue
@@ -51,7 +51,7 @@ export default {
         // Prendi l'url dell'immagine
         getImageUrl(imageName) {
             console.log(imageName);
-            return `http://127.0.0.1:8000/storage/${imageName}`;
+            return `http://127.0.0.1:8001/storage/${imageName}`;
         },
 
         updateApartments(filtered) {
@@ -118,7 +118,7 @@ export default {
 
     // CHIAMATE AXIOS PER SERVIZI E APPARTAMENTI
     mounted() {
-        axios.get('http://127.0.0.1:8000/api/v1/')
+        axios.get('http://127.0.0.1:8001/api/v1/')
             .then(response => {
                 const data = response.data;
                 this.originalApartments = data;  // Store all data in originalApartments
@@ -128,7 +128,7 @@ export default {
                 console.log(error);
             });
 
-        axios.get('http://127.0.0.1:8000/api/v1/service')
+        axios.get('http://127.0.0.1:8001/api/v1/service')
             .then(response => {
                 const data = response.data;
                 this.services = data;
